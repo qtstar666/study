1f626c9106fd89023ed3799f764910241d7494f7
qtstar <qt1314i@163.com>
2022-04-25 18:03:25 +0800

    创建图书、与书籍信息数据库。尝试添加数据

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -2,10 +2,40 @@ from django.db import models
 
 
 # Create your models here.
+# 创建图书表
 class BookInfo(models.Model):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=20, verbose_name='名称')
+    pub_date = models.DateField(verbose_name='发布日期', null=True)
+    readcount = models.IntegerField(default=0, verbose_name='阅读量')
+    commentcount = models.IntegerField(default=0, verbose_name='评论量')
+    is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    # 重命名表名
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '图书'
+
+    # 定义数据对象显示信息
+    def __str__(self):
+        return self.name
 
 
+# 创建人物信息表，是图书表的外键
 class PeopleInfo(models.Model):
-    name = models.CharField(max_length=10)
-    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female')
+    )
+
+    name = models.CharField(max_length=20, verbose_name='名称')
+    gender = models.SmallIntegerField(choices=GENDER_CHOICES, default='0', verbose_name='性别')
+    description = models.CharField(max_length=200, null=True, verbose_name='描述信息')
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE, verbose_name='图书')
+    is_delete = models.BooleanField(default='False', verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人物信息'
+
+    def __str__(self):
+        return self.name
--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from book.models import PeopleInfo, BookInfo
+
+BookInfo.objects.create(
+    name='三国演义'
+
+)
+
+PeopleInfo.objects.create(
+    name='刘备',
+    book_id='5'
+)
+PeopleInfo.objects.create(
+    name='关羽',
+    book_id=5
+)
+
 
 def index(request):
     return HttpResponse("OK")
